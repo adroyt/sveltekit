b7cc23edb2dc15947dec04c2c3a89652588c3003
Haniel Ubogu <hanieltobi@gmail.com>
2022-10-19 15:19:38 +0100

    updated boilerplate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,18 +7,10 @@
 
 <div class="transition-400 flex h-full select-none text-center">
   <div class="m-auto">
-    <div class="fw-100 animate-count-infinite duration-1s animate-bounce text-5xl">
-      unocss & sveltekit
-    </div>
-
-    <div class="op-30 fw-300 max-w-40ch mb-2 text-lg">
-      The fastest way to build svelte apps with the Instant on-demand Atomic CSS engine
-    </div>
-
     <!-- svelte-ignore a11y-missing-content -->
     <div class="m-2 flex h-max items-center justify-center gap-2 text-2xl">
       <a
-        class="i-logos-unocss hover:brightness-300% text-inherit"
+        class="i-logos-unocss transition-200 brightness-200% hover:brightness-150% text-inherit"
         href="https://uno.antfu.me/"
         target="_blank"
       />
@@ -26,10 +18,14 @@
       <div class="text-2rem op-30">+</div>
       <!-- demarcation end -->
       <a
-        class="i-carbon-logo-svelte op-30 hover:op-80 text-inherit"
+        class="i-logos-svelte-kit transition-200 hover:op-80 text-40 text-inherit"
         href="https://kit.svelte.dev"
         target="_blank"
       />
+    </div>
+
+    <div class="op-30 fw-300 max-w-40ch mb-2 -mt-14 text-lg">
+      The fastest way to build svelte apps with the Instant on-demand Atomic CSS engine
     </div>
   </div>
 </div>
