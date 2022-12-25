5a7a50210d0fcd42a2ff622d4fe3a7f24d134277
Haniel Ubogu <hanieltobi@gmail.com>
2022-12-25 21:37:50 +0100

    changed uno.config.ts & bumped unocss

--- uno.config.ts
+++ uno.config.ts
@@ -8,6 +8,8 @@ import {
   transformerVariantGroup,
 } from "unocss";
 
+import { handler as h, variantGetParameter } from "@unocss/preset-mini/utils";
+
 // https://github.com/unocss/unocss/tree/main/packages/vite
 // https://github.com/unocss/unocss/tree/main/packages/vite#svelte
 // https://github.com/unocss/unocss/tree/main/packages/preset-uno
@@ -27,7 +29,7 @@ export default defineConfig({
   rules: [],
 
   // https://github.com/unocss/unocss#shortcuts
-  shortcuts: {},
+  shortcuts: [],
 
   preflights: [
     {
@@ -42,6 +44,71 @@ export default defineConfig({
         height: 100%;
       }
       `,
+    },
+  ],
+
+  variants: [
+    {
+      // adds support for "@min-[width]:class" and "@min-h-[width]:class"
+      // or
+      // "@min-width:class" and "@min-h-width:class"
+      name: "arbitrary-media-query",
+      match(matcher) {
+        // prefix with @ to specify that it's a media query
+        const minVariant = variantGetParameter("@min-", matcher, [":"]);
+        const maxVariant = variantGetParameter("@max-", matcher, [":"]);
+        const minHeightVariant = variantGetParameter("@min-h-", matcher, [":"]);
+        const maxHeightVariant = variantGetParameter("@max-h-", matcher, [":"]);
+
+        // the order that we check the variants is important
+        // because we want to match the most specific one
+        const matched =
+          (minHeightVariant && {
+            type: "min-h",
+            variant: minHeightVariant,
+          }) ||
+          (maxHeightVariant && {
+            type: "max-h",
+            variant: maxHeightVariant,
+          }) ||
+          (minVariant && {
+            type: "min",
+            variant: minVariant,
+          }) ||
+          (maxVariant && {
+            type: "max",
+            variant: maxVariant,
+          });
+
+        if (matched?.variant) {
+          const [match, rest] = matched.variant;
+          // this is for extracting the value from the match and
+          // makes sure it either has no brackets or has brackets
+          const extractedValue =
+            h.bracket(match) || (!match.startsWith("[") && !match.endsWith("]") && match) || "";
+          const endsWithUnit = /^\d+(em|px|rem)$/.test(extractedValue);
+          const isOnlyNum = /^\d+$/.test(extractedValue);
+
+          if (endsWithUnit || isOnlyNum) {
+            return {
+              matcher: rest,
+              handle: (input, next) =>
+                next({
+                  ...input,
+                  parent: `${input.parent ? `${input.parent} $$ ` : ""}@media (${
+                    matched.type == "min"
+                      ? "min-width"
+                      : matched.type == "max"
+                      ? "max-width"
+                      : matched.type == "min-h"
+                      ? "min-height"
+                      : "max-height"
+                  }:${endsWithUnit ? extractedValue : extractedValue + "px"})`,
+                }),
+            };
+          }
+        }
+      },
     },
   ],
 
