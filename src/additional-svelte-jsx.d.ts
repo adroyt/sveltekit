f1c433fd575eb5173aee00bcf4ec5df17863d0ac
stoydev <stoydev@stoysolutions.com>
2023-10-07 11:09:15 +0100

    removed unnecesary file

--- src/additional-svelte-jsx.d.ts
+++
@@ -1,9 +0,0 @@
-import type { AttributifyAttributes } from "@unocss/preset-attributify";
-
-declare global {
-  namespace svelte.JSX {
-    interface HTMLAttributes<T> extends AttributifyAttributes<T> {
-      [key: string]: unknown;
-    }
-  }
-}
